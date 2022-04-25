1a14c8b90e15ab1ca68d189466d633aa24f50941
Alexey Kulish <alexkulish1993@gmail.com>
2022-04-25 12:11:54 +0300

    Initial Commit

---
+++ Armagedon2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Armagedon2022
+//
+//  Created by Alex Kulish on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
